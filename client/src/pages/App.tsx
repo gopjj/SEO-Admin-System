e82d076badfb53f938d8de4f9b2dd84feeb06259
Yu Xiao <xiaoyu@microsoft.com>
2023-09-03 17:21:13 +0800

    feat: organize project & add upload component

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -9,8 +9,7 @@ import {
 import type { MenuProps } from "antd";
 import { Layout, Menu, theme } from "antd";
 import { Link, Outlet } from "react-router-dom";
-import logo from "../images/logo.png";
-
+import logo from "../component/images/logo.png";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -42,7 +41,7 @@ const items: MenuItem[] = [
     getItem("Team 1", "6"),
     getItem("Team 2", "8"),
   ]),
-  getItem("上传文件", "9", <FileOutlined />),
+  getItem(<Link to="upload">上传文件</Link>, "9", <FileOutlined />),
 ];
 
 const App: React.FC = () => {
@@ -53,30 +52,30 @@ const App: React.FC = () => {
 
   return (
     <Layout>
-      <Header style={{ display: "flex", alignItems: "center" ,background: "white"}}>
-      <img src={logo} alt="Logo" style={{ height: 32, margin: -20 }} />
-      
+      <Header
+        style={{ display: "flex", alignItems: "center", background: "white" }}
+      >
+        <img src={logo} alt="Logo" style={{ height: 32, margin: -20 }} />
       </Header>
-      <Layout style={{ minHeight: "100vh", }}>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={(value) => setCollapsed(value)}
-        theme="light"
-        style={{ background: "#FFFFFF" }} // 修改为您想要的背景色
-    >
-  <div className="demo-logo-vertical" />
-  <Menu
-    theme="light"
-   // style={{ width: 200}}
-    defaultSelectedKeys={["1"]}
-    mode="inline"
-    items={items}
-  />
-</Sider>
- 
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+          theme="light"
+          style={{ background: "#FFFFFF" }} // 修改为您想要的背景色
+        >
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="light"
+            // style={{ width: 200}}
+            defaultSelectedKeys={["1"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+
         <Layout>
-        
           <Content style={{ margin: "0 16px" }}>
             <Link to="option1"></Link>
             <Link to="option2"></Link>
--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Option1 from "../pages/Option1";
+import Upload from "../pages/Upload";
 import Option2 from "../pages/Option2";
 import App from "../pages/App";
 import Dashboard from "../pages/dashboard/dashboard";
@@ -9,9 +9,9 @@ const Router: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route path="option1" element={<Option1 />}></Route>
+          <Route path="upload" element={<Upload />}></Route>
           <Route path="option2" element={<Option2 />}></Route>
-          <Route path="dashboard" element={<Dashboard/>}></Route>
+          <Route path="dashboard" element={<Dashboard />}></Route>
         </Route>
         <Route path="/404" element={<div>404 Not Found</div>}></Route>
       </Routes>
